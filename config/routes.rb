12004b482ef3ba3496bd70634d3012df81bfc74e
Ktsunezawa <rapurasnohako@gmail.com>
2021-01-08 09:39:43 +0000

    Finish 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :blogs
+  delete  'blogs/:id' => 'blogs#destroy',as: 'destroy_blog'
 end
